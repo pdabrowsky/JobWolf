e0969d835d995426fd5b3162b71e90de0f3399a7
Pawel <rullox13@gmail.com>
2023-11-02 22:49:14 +0100

    feat: add toastify notifications

---
+++ components/atoms/ToastContainer/ToastContainer.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { ToastContainer, toast, ToastClassName } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+const contextClass = {
+  success: 'bg-green-900',
+  error: 'bg-red-900',
+  info: 'bg-stone-600',
+  warning: 'bg-yellow-700',
+  default: 'bg-stone-600',
+}
+
+const getToastClassName: ToastClassName = (context = {}) => {
+  const { type } = context
+  const className = contextClass[type || 'default']
+  return `${className} relative flex px-2 py-3 text-[14px] lg:text-[15px] mb-2 min-h-10 lg:min-h-14 lg:rounded-md justify-between overflow-hidden cursor-pointer`
+}
+
+export const ToastContainerWrapper = () => (
+  <ToastContainer
+    position={toast.POSITION.TOP_RIGHT}
+    hideProgressBar
+    theme="colored"
+    autoClose={3500}
+    toastClassName={getToastClassName}
+  />
+)
