3394b2fe2fe28b5835e72eab4942b775f362c548
Pawel <rullox13@gmail.com>
2023-11-08 13:37:36 +0100

    fix: console error typo

--- icons/ProfileIcon.tsx
+++ icons/ProfileIcon.tsx
@@ -10,13 +10,13 @@ export const ProfileIcon = forwardRef<SVGSVGElement, SvgIconProps>(
           cy="10"
           r="3"
           stroke="currentColor"
-          stroke-linecap="round"
+          strokeLinecap="round"
         />
         <circle cx="12" cy="12" r="9" stroke="currentColor" />
         <path
           d="M18 18.7059C17.6461 17.6427 16.8662 16.7033 15.7814 16.0332C14.6966 15.3632 13.3674 15 12 15C10.6326 15 9.30341 15.3632 8.21858 16.0332C7.13375 16.7033 6.35391 17.6427 6 18.7059"
           stroke="currentColor"
-          stroke-linecap="round"
+          strokeLinecap="round"
         />
       </SvgIcon>
     )
