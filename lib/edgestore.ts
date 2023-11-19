571c0bd2429a7f855a7563448e4fffae4f7403fd
Pawel <rullox13@gmail.com>
2023-11-19 13:53:55 +0100

    chore: add react-dropzone, configure max currentuploads

--- lib/edgestore.ts
+++ lib/edgestore.ts
@@ -3,7 +3,7 @@
 import { type EdgeStoreRouter } from '../app/api/edgestore/[...edgestore]/route'
 import { createEdgeStoreProvider } from '@edgestore/react'
 
-const { EdgeStoreProvider, useEdgeStore } =
-  createEdgeStoreProvider<EdgeStoreRouter>()
-
-export { EdgeStoreProvider, useEdgeStore }
+export const { EdgeStoreProvider, useEdgeStore } =
+  createEdgeStoreProvider<EdgeStoreRouter>({
+    maxConcurrentUploads: 2,
+  })
