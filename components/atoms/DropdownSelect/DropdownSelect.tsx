b63507ea1cd84658a3fdf3f79365f78363115078
Pawel <rullox13@gmail.com>
2023-11-19 12:32:05 +0100

    fix: update max dropdown height, style tweak

--- components/atoms/DropdownSelect/DropdownSelect.tsx
+++ components/atoms/DropdownSelect/DropdownSelect.tsx
@@ -42,7 +42,7 @@ export const DropdownSelect = ({
   const error = name ? (errors?.[name] as FieldError) || undefined : undefined
 
   return (
-    <div className={cn(className, 'relative w-fit')}>
+    <div className={cn(className, 'relative w-full')}>
       <label htmlFor={name} className="block text-[12px] lg:text-[13px] pb-1">
         {label}
       </label>
@@ -55,7 +55,7 @@ export const DropdownSelect = ({
           placeholder={placeholder}
           onFocus={onFocus}
           onBlur={onBlur}
-          className="cursor-pointer bg-darkLight border border-borderMid text-gray px-1.5 py-1.5 text-[14px] lg:text-[16px] rounded-md placeholder:text-gray placeholder:text-opacity-30 hover:border-borderLight focus-visible:outline-none focus-visible:ring-0 focus-visible:border-borderLight"
+          className="cursor-pointer w-full bg-darkLight border border-borderMid text-gray px-1.5 py-1.5 text-[14px] lg:text-[16px] rounded-md placeholder:text-gray placeholder:text-opacity-30 hover:border-borderLight focus-visible:outline-none focus-visible:ring-0 focus-visible:border-borderLight"
           {...rest}
         />
         <ArrowDownIcon className=" h-6 absolute right-2 top-1/2 transform -translate-y-1/2 pointer-events-none" />
@@ -67,7 +67,7 @@ export const DropdownSelect = ({
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.25 }}
-            className="absolute mt-2 bg-darkLight rounded-md shadow-lg border border-borderLight z-10 w-full"
+            className="absolute mt-2 bg-darkLight rounded-md shadow-lg border border-borderLight z-10 w-full max-h-[140px] overflow-y-auto"
           >
             {options.map((option) => (
               <motion.li
