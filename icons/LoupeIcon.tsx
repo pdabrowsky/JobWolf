af5380a98a928cd78c3407944f18b069c458b8db
Pawel <rullox13@gmail.com>
2023-12-05 20:21:25 +0100

    feat: add loupe icon

---
+++ icons/LoupeIcon.tsx
@@ -0,0 +1,18 @@
+import { SvgIconProps, SvgIcon } from '@/components/atoms/SvgIcon'
+import { forwardRef } from 'react'
+
+export const LoupeIcon = forwardRef<SVGSVGElement, SvgIconProps>(
+  function LoupeIcon(props, ref) {
+    return (
+      <SvgIcon ref={ref} viewBox="0 0 24 24" fill="none" {...props}>
+        <circle cx="11" cy="11" r="7" stroke="currectColor" stroke-width="2" />
+        <path
+          d="M20 20L17 17"
+          stroke="currectColor"
+          stroke-width="2"
+          stroke-linecap="round"
+        />
+      </SvgIcon>
+    )
+  }
+)
