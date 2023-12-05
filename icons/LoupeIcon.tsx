9efe50ebce36343786b38f9ee2da1318637d3127
Pawel <rullox13@gmail.com>
2023-12-05 21:15:05 +0100

    fix: icon console error

--- icons/LoupeIcon.tsx
+++ icons/LoupeIcon.tsx
@@ -5,7 +5,7 @@ export const LoupeIcon = forwardRef<SVGSVGElement, SvgIconProps>(
   function LoupeIcon(props, ref) {
     return (
       <SvgIcon ref={ref} viewBox="0 0 24 24" fill="none" {...props}>
-        <circle cx="11" cy="11" r="7" stroke="currectColor" stroke-width="2" />
+        <circle cx="11" cy="11" r="7" stroke="currectColor" strokeWidth="2" />
         <path
           d="M20 20L17 17"
           stroke="currectColor"
