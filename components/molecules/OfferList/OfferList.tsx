bc5fddbd61c82ea57a44bc3d290e38ffcb3126dc
Pawel <rullox13@gmail.com>
2023-12-07 22:08:01 +0100

    feat: wip offer list

---
+++ components/molecules/OfferList/OfferList.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { OfferCard } from '@/components/atoms/OfferCard'
+import { OfferListProps } from './OfferList.types'
+import { cn } from '@/lib/utils'
+
+export const OffersList = ({ offers, className }: OfferListProps) => {
+  return (
+    <div className={cn('flex flex-col gap-4', className)}>
+      {offers.map((offer) => (
+        <OfferCard key={offer.id} {...offer} />
+      ))}
+    </div>
+  )
+}
---
+++ components/molecules/OfferList/OfferList.types.ts
@@ -0,0 +1,6 @@
+import { OfferCardProps } from '@/components/atoms/OfferCard'
+
+export type OfferListProps = {
+  offers: OfferCardProps[]
+  className?: string
+}
