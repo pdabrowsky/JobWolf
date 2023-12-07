5b424cdc52b83f0bd638f57f170df2bc5d74dcc6
Pawel <rullox13@gmail.com>
2023-12-07 22:02:29 +0100

    feat: add getOffer query

---
+++ app/actions/offer/getOfferList.tsx
@@ -0,0 +1,50 @@
+import prisma from '@/lib/prisma'
+
+export const getOfferList = async (query?: string) => {
+  try {
+    let whereCondition = {}
+
+    if (query && query.trim() !== '') {
+      whereCondition = {
+        title: {
+          contains: query,
+        },
+      }
+    }
+
+    const offers = await prisma.offer.findMany({
+      where: whereCondition,
+      include: {
+        employer: {
+          select: {
+            logoUrl: true,
+            logoName: true,
+            name: true,
+          },
+        },
+        mustHaveTech: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    return offers.map((offer) => {
+      return {
+        id: offer.id,
+        title: offer.title,
+        technologies: offer.mustHaveTech.map((tech) => tech.name),
+        companyName: offer.employer.name || '',
+        employerLogoUrl: offer.employer.logoUrl || '',
+      }
+    })
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
