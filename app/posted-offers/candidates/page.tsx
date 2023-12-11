554e365798290e603a743e3c91ad40c59c0b8992
Pawel <rullox13@gmail.com>
2023-12-11 20:04:33 +0100

    feat: add posted offers candidates

---
+++ app/posted-offers/candidates/page.tsx
@@ -0,0 +1,26 @@
+import { getOfferCandidates } from '@/app/actions/offer/getOfferCandidates'
+import { CandidatesExplorer } from '@/components/organisms/CandidatesExplorer'
+import { notFound } from 'next/navigation'
+
+const PostedOfferCandidatesPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) => {
+  const offerId =
+    typeof searchParams.offer === 'string' ? searchParams.offer : undefined
+
+  if (!offerId) return notFound()
+
+  const candidates = await getOfferCandidates(offerId)
+
+  return (
+    <div className="flex flex-col gap-12 items-center justify-center pt-12 w-full px-4">
+      <h1 className="text-[16px] mr-auto lg:m-auto lg:text-2xl font-semibold lg:min-w-[800px]">
+        Applicants for this position
+      </h1>
+      <CandidatesExplorer candidates={candidates} />
+    </div>
+  )
+}
+export default PostedOfferCandidatesPage
