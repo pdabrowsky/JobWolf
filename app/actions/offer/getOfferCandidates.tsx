7b90ea209f45f61471222479f134174d0166a202
Pawel <rullox13@gmail.com>
2023-12-11 20:04:07 +0100

    feat: add get offer candidates

---
+++ app/actions/offer/getOfferCandidates.tsx
@@ -0,0 +1,81 @@
+import prisma from '@/lib/prisma'
+
+export const getOfferCandidates = async (
+  offerId: string,
+  candidateName?: string
+) => {
+  try {
+    const whereClause: {
+      offerId: string
+      candidate?: {
+        OR: Array<{
+          firstName?: {
+            contains: string
+            mode: 'insensitive'
+          }
+          lastName?: {
+            contains: string
+            mode: 'insensitive'
+          }
+        }>
+      }
+    } = {
+      offerId: offerId,
+    }
+
+    if (candidateName) {
+      whereClause.candidate = {
+        OR: [
+          {
+            firstName: {
+              contains: candidateName,
+              mode: 'insensitive',
+            },
+          },
+          {
+            lastName: {
+              contains: candidateName,
+              mode: 'insensitive',
+            },
+          },
+        ],
+      }
+    }
+
+    const applications = await prisma.application.findMany({
+      where: whereClause,
+      include: {
+        candidate: {
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+            email: true,
+            phone: true,
+            description: true,
+            githubUrl: true,
+            portfolioUrl: true,
+            fileName: true,
+            fileUrl: true,
+          },
+        },
+      },
+    })
+
+    return applications.map((application) => ({
+      id: application.candidate.id,
+      firstName: application.candidate.firstName,
+      lastName: application.candidate.lastName,
+      email: application.candidate.email,
+      phone: application.candidate.phone,
+      description: application.candidate.description,
+      githubUrl: application.candidate.githubUrl,
+      portfolioUrl: application.candidate.portfolioUrl,
+      fileName: application.candidate.fileName,
+      fileUrl: application.candidate.fileUrl,
+    }))
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
