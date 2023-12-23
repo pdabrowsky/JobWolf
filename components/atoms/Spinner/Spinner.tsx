c9ee6742df2819a9452c4d9e72039094a461c6e8
Pawel <rullox13@gmail.com>
2023-12-23 16:41:24 +0100

    fix: improve spinner accessibility

--- components/atoms/Spinner/Spinner.tsx
+++ components/atoms/Spinner/Spinner.tsx
@@ -4,13 +4,20 @@ import { cn } from '@/lib/utils'
 
 export const Spinner = ({ className }: SpinnerProps) => {
   return (
-    <div className="flex justify-center items-center">
+    <div
+      className="flex justify-center items-center"
+      role="status"
+      aria-live="polite"
+      aria-busy="true"
+    >
       <div
         className={cn(
           'animate-spin rounded-full h-6 w-6 border-b-2 border-gray-900',
           className
         )}
-      ></div>
+      >
+        <span className="sr-only">Loading...</span>
+      </div>
     </div>
   )
 }
