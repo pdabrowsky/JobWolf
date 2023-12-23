5153924fc2c780f80ce7b73dac27da4be0367e5a
Pawel <rullox13@gmail.com>
2023-12-23 21:56:06 +0100

    fix: style tweak

--- app/post-job/page.tsx
+++ app/post-job/page.tsx
@@ -4,7 +4,7 @@ import { getAllJobFormOptions } from '../actions/offer/getFormData'
 const ProfilePage = async () => {
   const options = await getAllJobFormOptions()
   return (
-    <div className="flex justify-center mt-10 lg:mt-20 px-5">
+    <div className="flex justify-center my-10 lg:my-20 px-5">
       <PostJobForm selectOptions={options} />
     </div>
   )
