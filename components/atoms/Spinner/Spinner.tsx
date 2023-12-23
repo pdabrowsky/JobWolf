eb4ba586c5a02d73a5c7555fe8b0d7b31e921a35
Pawel <rullox13@gmail.com>
2023-12-23 16:57:23 +0100

    fix: add ref to spinner

--- components/atoms/Spinner/Spinner.tsx
+++ components/atoms/Spinner/Spinner.tsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { SpinnerProps } from './Spinner.types'
 import { cn } from '@/lib/utils'
 
-export const Spinner = ({ className }: SpinnerProps) => {
-  return (
-    <div
-      className="flex justify-center items-center"
-      role="status"
-      aria-live="polite"
-      aria-busy="true"
-    >
+export const Spinner = forwardRef<HTMLDivElement, SpinnerProps>(
+  ({ className }, ref) => {
+    return (
       <div
-        className={cn(
-          'animate-spin rounded-full h-6 w-6 border-b-2 border-gray-900',
-          className
-        )}
+        ref={ref}
+        className="flex justify-center items-center"
+        role="status"
+        aria-live="polite"
+        aria-busy="true"
       >
-        <span className="sr-only">Loading...</span>
+        <div
+          className={cn(
+            'animate-spin rounded-full h-6 w-6 border-b-2 border-gray-900',
+            className
+          )}
+        >
+          <span className="sr-only">Loading...</span>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
+
+Spinner.displayName = 'Spinner'
