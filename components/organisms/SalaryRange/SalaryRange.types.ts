717ef9823f049ec69901befcc598b3af4a2c7524
Pawel <rullox13@gmail.com>
2023-12-27 17:24:07 +0100

    fix: update type

--- components/organisms/SalaryRange/SalaryRange.types.ts
+++ components/organisms/SalaryRange/SalaryRange.types.ts
@@ -1,12 +1,12 @@
 import { Control, FieldErrors, UseFormRegister } from 'react-hook-form'
 import { PostJobFormInput } from '../PostJobForm'
-import { SelectOption } from '../PostJobForm/PostJobForm.types'
+import { CheckboxOption } from '@/components/molecules/CheckboxGroup'
 
 export type SalaryRangeProps = {
   className?: string
   name: string
   control: Control<PostJobFormInput>
   register: UseFormRegister<PostJobFormInput>
-  contractTypes: SelectOption[]
+  contractTypes: CheckboxOption[]
   formErrors?: FieldErrors<PostJobFormInput>
 }
