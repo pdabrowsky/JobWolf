71d0ebf9679319517311a8ffb02afa183ac0e170
Pawel <rullox13@gmail.com>
2023-12-27 21:45:29 +0100

    fix: only candidate can visible chat ai

--- app/offer/page.tsx
+++ app/offer/page.tsx
@@ -11,6 +11,7 @@ const OfferPage = async ({
   searchParams: { [key: string]: string | string[] | undefined }
 }) => {
   const session = await getServerSession(authOptions)
+  const isCandidate = session?.user?.role === 'Candidate'
 
   const offerId =
     typeof searchParams.id === 'string' ? searchParams.id : undefined
@@ -23,7 +24,7 @@ const OfferPage = async ({
   return (
     <div className="flex justify-center my-10 lg:my-20 px-5">
       <OfferDetails {...offer} />
-      {session?.user?.email && <ChatAI offerDetails={offer} />}
+      {isCandidate && <ChatAI offerDetails={offer} />}
     </div>
   )
 }
