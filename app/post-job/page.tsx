2f351800fef580897a64ba0b61be50e4716956ad
Pawel <rullox13@gmail.com>
2023-12-27 21:45:06 +0100

    fix: protect post a job page

--- app/post-job/page.tsx
+++ app/post-job/page.tsx
@@ -1,13 +1,45 @@
 import { PostJobForm } from '@/components/organisms/PostJobForm'
 import { getFilltersOptions } from '../actions/offer/getFilterOptions'
+import { getServerSession } from 'next-auth'
+import { UserRole } from '../actions/types'
+import { notFound } from 'next/navigation'
+import { authOptions } from '../api/auth/[...nextauth]/route'
+import { getEmployerProfile } from '../actions/employer/profile'
+import { routes } from '@/constants/routes'
+import Link from 'next/link'
 
-const ProfilePage = async () => {
-  const options = await getFilltersOptions()
+const PostJobPage = async () => {
+  const session = await getServerSession(authOptions)
+
+  if (session?.user?.role !== UserRole.Employer || !session?.user?.email) {
+    notFound()
+  }
+  const { data } = await getEmployerProfile(session.user.email)
+  const isProfileFilled =
+    data?.name &&
+    data?.city &&
+    data?.address &&
+    data?.phone &&
+    data?.description &&
+    data?.logoUrl
+
+  let options
+  if (isProfileFilled) options = await getFilltersOptions()
+
   return (
     <div className="flex justify-center my-10 lg:my-20 px-5">
-      <PostJobForm selectOptions={options} />
+      {isProfileFilled && options ? (
+        <PostJobForm selectOptions={options} />
+      ) : (
+        <h2 className="text-lg text-center">
+          To post an offer, please complete your profile
+          <Link className="text-gold mx-1" href={routes.PROFILE}>
+            here
+          </Link>
+        </h2>
+      )}
     </div>
   )
 }
 
-export default ProfilePage
+export default PostJobPage
